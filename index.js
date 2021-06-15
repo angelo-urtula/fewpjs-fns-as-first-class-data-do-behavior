72a17ed4c2def736d47051fba5e4685c1da12c7a
angelo <aurtula1@gmail.com>
2021-06-15 10:27:34 -0400

    commit tree 787ac8146a544ff74c8715f261edc5a69b71c28e

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString){
+  const timeArray = timeString.split(':')
+  if (parseInt(timeArray[0]) < 12){
+    return "Good Morning"
+  } else if (parseInt(timeArray[0]) > 12 && parseInt(timeArray[0]) < 17) {
+    return "Good Afternoon"
+  } else if (parseInt(timeArray[0]) > 17) {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string){
+  const greet = document.querySelector('#greeting')
+  greet.innerText = string
+}
+
